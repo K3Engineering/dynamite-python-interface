c4d564cff0d671666c3aa92a25f33684eb59a055
vladkvit <vladkvit@outlook.com>
2024-12-03 13:53:59 -0500

    Remove counter from publish

--- publish.py
+++ publish.py
@@ -2,15 +2,12 @@ import asyncio
 
 
 async def publish_messages(message_queue, parsed_bt_queue, shutdown_event):
-    counter = 0
-    while not shutdown_event.is_set() and counter < 100:
+    while not shutdown_event.is_set():
         try:
-            message = await asyncio.wait_for(parsed_bt_queue.get(), timeout=0.5)
+            message = await asyncio.wait_for(parsed_bt_queue.get(), timeout=1)
             await message_queue.put(message)
 
         except asyncio.TimeoutError:
             continue
 
-        await asyncio.sleep(1)
-        counter += 1
     print("shutting down publisher")
