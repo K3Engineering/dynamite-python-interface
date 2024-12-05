753a9712380d79b85a3fddd34c35d4ae67b8da8d
vladkvit <vladkvit@outlook.com>
2024-12-05 10:57:45 -0500

    Get rid of publisher, simplify code

--- main.py
+++ main.py
@@ -2,30 +2,25 @@ import asyncio
 import threading
 import signal
 from time import sleep
-from publish import publish_messages
 from subscribe import subscribe_to_messages
 from chart_plotter import plotter, update_data
 from writer import write_to_file
 from bt import bt_setup
 
-message_queue = asyncio.Queue()
 parsed_bt_queue = asyncio.Queue()
 shutdown_event = threading.Event()
 
 
 async def main():
-    publisher_task = asyncio.create_task(
-        publish_messages(message_queue, parsed_bt_queue, shutdown_event)
-    )
     subscriber_task = asyncio.create_task(
         subscribe_to_messages(
-            message_queue, shutdown_event, [update_data, write_to_file]
+            parsed_bt_queue, shutdown_event, [update_data, write_to_file]
         )
     )
 
     await bt_setup(parsed_bt_queue, shutdown_event)
 
-    await asyncio.gather(publisher_task, subscriber_task, return_exceptions=True)
+    await asyncio.gather(subscriber_task, return_exceptions=True)
 
 
 def run_event_loop(loop):
