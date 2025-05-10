1821002d4a6a4b4faeee4c4a120495b4a5e02c9b
Larry <larry@k3engineering.com>
2025-05-10 15:52:05 -0400

    Removing old JS script, adding a few comments

--- read_from_tcp_4_ports.js
+++ read_from_tcp_4_ports.js
@@ -9,7 +9,7 @@ if(!('Scope' in this)) throw "Please open a Scope instrument";
 var ports = [8080, 8081, 8082, 8083];
 var servers = [Tcp(),Tcp(),Tcp(),Tcp()];
 
-//var server = Tcp();
+// Connect to all ports
 for( var i = 0; i < 4; i++){
     print("server num", i, ports[i]);
     if(!servers[i].listen("localhost", ports[i])) throw "server error";
@@ -17,7 +17,7 @@ for( var i = 0; i < 4; i++){
     if(!servers[i].isConnected() && !servers[i].waitForNewConnection(20.0)) throw "server wait timeout";
 }
 
-// The first TCP sends the scaling factor to use
+// The first TCP int is the scaling factor to convert the readings.
 wait(0.1);
 var scaleFactor = [1,1,1,1];
 for( var i = 0; i < 4; i++) {
@@ -26,7 +26,6 @@ for( var i = 0; i < 4; i++) {
 }
 
 
-//channelData = [];
 channelDatas = [[],[],[],[]];
 scopes = [Scope.Ref1, Scope.Ref2, Scope.Ref3, Scope.Ref4];
 maxLenData = 8000; // How many values to buffer
@@ -37,18 +36,18 @@ while(1){
 
     for( var i = 0; i < 4; i++)
     {
-        
+        // Read all data on the socket
         var inputVals = servers[i].readInt(0);
 
         // Scale each value
         for( var inputI = 0; inputI < inputVals.length; inputI++) {
             inputVals[inputI] /= scaleFactor[i];
         }
-        //inputVals.map(element => element / scaleFactor);
     
+        // Extend the data
         Array.prototype.push.apply(channelDatas[i], inputVals);
     
-        // keep only the latest data, and remove old data
+        // Trim - keep only the latest data, and remove old data
         while(channelDatas[i].length > maxLenData){
             channelDatas[i].shift();
         }
