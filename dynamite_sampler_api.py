2c972ce81b89fb3fb23bab4a3eb91959911ee2d7
Vlad K <2459295+vladkvit@users.noreply.github.com>
2026-08-07 10:29:34 -0400

    Add a HardwareRevision BLE characteristic

--- dynamite_sampler_api.py
+++ dynamite_sampler_api.py
@@ -239,6 +239,15 @@ class DeviceInfo(BLEService):
         def unpack(b: bytearray | bytes) -> str:
             return str(b, "utf-8")
 
+    class HardwareRevision(BLECharacteristicRead[str]):
+        """Board model, e.g. 'v700P'"""
+
+        UUID = "2A27"
+
+        @staticmethod
+        def unpack(b: bytearray | bytes) -> str:
+            return bytes(b).rstrip(b"\x00").decode("utf-8")
+
     class TxPowerLevel(BLECharacteristicRead[int]):
         UUID = "2A07"
 
